6accff89b714f56fb6e9963e6f987bd4ded7762d
rishav-sharma-vst-au4 <54503782+rishav-sharma-vst-au4@users.noreply.github.com>
2020-11-06 03:40:41 +0530

    login

--- client/src/components/pages/login.jsx
+++ client/src/components/pages/login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import "./login.css"
 
 const login = () => {
     return (
-        <div>
-            <h1>login</h1>
+        <div className="logincard">
+            <div className='card auth-card'>
+                <h2>MeImg</h2>
+                <h4>Waiting To See You In</h4>
+                <div className="e_mail">
+                <input
+                id="email"
+                type="email"
+                placeholder="Input your E-mail here"
+                className="validate"
+                />
+                </div>
+                <div className="passwords">
+                    <input
+                    id="password"
+                    type="password"
+                    placeholder="Enter your Password here"
+                    className="validate"/>
+                </div>
+                <button className="btn waves-effect waves-light" type="submit" name="action">Login
+                    <i className="material-icons right">send</i>
+                </button>
+            </div>
         </div>
     );
 };
